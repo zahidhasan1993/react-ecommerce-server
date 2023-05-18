59f920e3fb3bd1edbab1e0fa2e4973d40d8d6ae5
devZahid <officialzahidhasan19932021@gmail.com>
2023-05-18 13:31:10 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//middle ware
+
+app.use(cors());
+app.use(express.json());
+
+//DB connection
+
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASS}@cluster0.d09ztu7.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+//server routes
+
+app.get('/', (req,res) => {
+    res.send('WELCOME TO REACT_ECOMMERCE SERVER');
+})
+
+app.listen(port,() => {
+    console.log('app listning on port',port);
+})
